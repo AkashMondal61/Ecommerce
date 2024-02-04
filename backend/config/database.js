7d979fe58ce24cec7150bf7c52e1922d21d16dfd
AkashMondal61 <95472132+AkashMondal61@users.noreply.github.com>
2024-02-04 12:09:33 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,7 @@ const connectDatabase=()=>
 {
 mongoose.connect(process.env.DB_URL)
 .then((data)=>{console.log(`database connected with server: ${data.connection.host}`)})
-//As we have handeled the 
-// .catch((error)=>{console.log(`error`)});
+
 }
 
 module.exports=connectDatabase; 
